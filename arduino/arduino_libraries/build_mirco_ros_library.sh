b5e87961773e9cb993a9c93e184b1afa30bbaed3
Jinhwan Moon <jhmoon0224@gmail.com>
2024-03-04 00:18:11 +0900

    fix build_micro_ros_libary.sh 1.

--- arduino/arduino_libraries/build_mirco_ros_library.sh
+++ arduino/arduino_libraries/build_mirco_ros_library.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+# change env for your ros package
 ROS_PKG=galactic
-MONIARM_SRC=~/ros2_ws/src/moniarm/
+MONIARM_SRC=~/ros2_ws/src/moniarm
+ARDUINO_ROOT=~/Arduino
 
+# 1. download micro_ros_arduino source code from github
 git clone -b $ROS_PKG https://github.com/micro-ROS/micro_ros_arduino
-cd micro_ros_arduino/extras/library_generation/extra_packages
 
-cp -r $MONIARM_SRC/moniarm_interfaces  ./micro_ros_arduino/extras/library_generation/extra_packages
+# 2. copy moniarm custom message, service interface package(moniarm_interfaces) from the Moniarm main interface directory to mirco_ros extra_packages directory.
+cp -r $MONIARM_SRC/moniarm_interfaces  ./micro_ros_arduino/extras/library_generation/extra_packages/
 
-ros2 pkg create --build-type ament_cmake uros_moniarm_interfaces
-
+# 3. build micro_ros_arduino with docker 
 cd micro_ros_arduino
-
 docker run -it --rm -v $(pwd):/project --env MICROROS_LIBRARY_FOLDER=extras microros/micro_ros_static_library_builder:$ROS_PKG -p esp32
+
+# 4.1 zip micro_ros_arduino for Arduino IDE libray
+cd ..
+#zip micro_ros_arduino.zip micro_ros_arduino
+
+# 4.2 copy micro_ros_arduino to Arduino libary
+# cp -r micro_ros_arduino  $ARDUINO_ROOT/libraries/
