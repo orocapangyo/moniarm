25555e8b2adc5415151736e41810fcc8b2a9d693
ChangWhan Lee <zeta0707@gmail.com>
2024-06-21 23:09:22 +0900

    Update motorScanner.ino
    1. change motor detection method, read memory
    2. Add motor ID change function

--- arduino/motorScanner/motorScanner.ino
+++ arduino/motorScanner/motorScanner.ino
@@ -1,5 +1,5 @@
 /*
- * DRS-0101 smart motor tester
+ * DRS-0101 motor ID scan and ID changer
  * ZETA7, zeta0707@gmail.com
 */
 #include <Herkulex.h>
@@ -17,6 +17,8 @@
 #define TXD2 17
 
 #define ALL_MOTOR 0xFE
+#define ADDR_ACCEL_RATIO 8
+#define DFT_ACCEL_RATIO 25
 
 // Time interval for measurements in milliseconds
 const int INTERVAL = 100;
@@ -25,7 +27,7 @@ long currentMillis = 0;
 
 bool blinkStatus = false;
 
-int motorID;
+int motorID = 0, oldID = 0, newID = 0;
 
 void setup() {
   // configure LED for output
@@ -41,31 +43,44 @@ void setup() {
   digitalWrite(BUZZER, LOW);
 
   Serial.begin(115200);  // Open serial communications for debug
-  Serial.println("Begin Herkulex ID scanner");
+  Serial.println("Herkulex ID scanner");
 
   Herkulex.beginSerial1(115200, RXD1, TXD1);  //open serial1 for motor communication
 
-  for (motorID = 0; motorID < 16; motorID++) {
+  for (motorID = 0; motorID < ALL_MOTOR; motorID++) {
     Herkulex.reboot(motorID);
-    delay(200);
   }
   Herkulex.initialize();  //initialize motors
-  for (motorID = 0; motorID < 16; motorID++) {
-    Herkulex.torqueOFF(motorID);
-  }
-  for (motorID = 0; motorID < 16; motorID++) {
+
+  for (motorID = 0; motorID < ALL_MOTOR; motorID++) {
+    if (Herkulex.readRegistryRAM(motorID, 8) == DFT_ACCEL_RATIO) {
       Serial.print("ID:");
-      Serial.print(motorID);
-      Serial.print(",Angle:");
-      Serial.println(Herkulex.getAngle(motorID));
+      Serial.println(motorID);
+    }
   }
 
-  Serial.println("Type motorID:");
+  Serial.println("Type oldID, newID:");
+  Serial.println("or Type checkID:");
 }
 
 void loop() {
   // Record the time
   currentMillis = millis();
+
+  //input ID angle
+  if (Serial.available() > 0) {
+    oldID = Serial.parseInt();
+    newID = Serial.parseInt();
+    if (newID != 0) {
+      Herkulex.set_ID(oldID, newID);
+      Serial.print(oldID);
+      Serial.print("->");
+      Serial.print(newID);
+    } else {
+      newID = oldID;
+      Serial.print("Watch green LED blink");
+    }
+  }
 
   // If the time interval has passed, receive command input
   // LED blink on ESP32, change color on Herkulex
@@ -73,20 +88,14 @@ void loop() {
     previousMillis = currentMillis;
 
     if (blinkStatus == false) {
-      Herkulex.setLed(motorID, LED_GREEN);
+      Herkulex.setLed(newID, LED_GREEN);
       blinkStatus = true;
     } else {
-      Herkulex.setLed(motorID, 0);
+      Herkulex.setLed(newID, 0);
       blinkStatus = false;
     }
 
     // blink LED to indicate activity
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-
-    //input ID angle
-    if (Serial.available() > 0) {
-      motorID = Serial.parseInt();
-      Serial.println("Watch Green LED blink");
-    }
   }
 }
