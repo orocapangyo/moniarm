bb4639df3e88ac30b59045cc34ba7fcbdfa0e98f
ChangWhan Lee <zeta0707@gmail.com>
2024-10-03 18:37:28 +0900

    Change Yolo object, need to update darknet_ros together
    1. Change Yolo object
    2. Add launch.py for taking picture at home position

---
+++ moniarm_control/launch/take_picture.launch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+  return LaunchDescription([
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([
+        FindPackageShare("moniarm_bringup"), '/launch', '/mcu.launch.py'])
+    ),
+
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([
+        FindPackageShare("moniarm_cv"), '/launch', '/usbcam.launch.py'])
+    ),
+
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([
+        FindPackageShare("moniarm_teleop"), '/launch', '/teleop_joy.launch.py'])
+    ),
+    
+  ])
